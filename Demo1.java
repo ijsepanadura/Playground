446204eb803894095f83fc68e424cae9e0118698
Ishan Lahiru <ishanlahiru2012@gmail.com>
2023-07-30 20:32:58 +0530

    Initial commit

---
+++ Demo1.java
@@ -0,0 +1,6 @@
+public class Demo1{
+    public static void main(String args[]){
+        float myFloat = 123456.789f;
+        System.out.printf("Your Account Balance is : $%.2f\n",myFloat);
+    }
+}
