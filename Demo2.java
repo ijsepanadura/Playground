4acf1dbda7cd311ea99f769b49695478d0f22b88
Ishan Lahiru <ishanlahiru2012@gmail.com>
2023-07-30 20:42:41 +0530

    Second commit

---
+++ Demo2.java
@@ -0,0 +1,7 @@
+public class Demo2 {
+    public static void main(String args[]){
+        final double constant = Math.PI;
+        System.out.printf("PI Value is : %.3f\n",constant);
+    }
+    
+}
