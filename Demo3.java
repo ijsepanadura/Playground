339527d6cfcc59a9fa6063b74fe91d4663f81f0c
Ishan Lahiru <ishanlahiru2012@gmail.com>
2023-07-30 20:49:59 +0530

    third commit

---
+++ Demo3.java
@@ -0,0 +1,8 @@
+public class Demo3 {
+    public static void main(String args[]){
+        int areaCode = 123;
+        int num1= 456, num2= 7890;
+        System.out.printf("Telephone number is : \033[32;7m(%d)\033[30;0m%d-%d\n",areaCode,num1,num2);
+    }
+    
+}
